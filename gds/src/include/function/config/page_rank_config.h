5a0e0be090959f86f1e90df0f7a36d8532df78e9
ziyi chen <chenziyi990424@gmail.com>
2025-04-28 15:55:15 +0800

    Support max iterations in wcc, scc (#5291)

---
+++ gds/src/include/function/config/page_rank_config.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+
+#include "common/exception/binder.h"
+#include "common/types/types.h"
+#include "function/gds/gds.h"
+
+namespace kuzu {
+namespace function {
+
+struct DampingFactor {
+    // The damping factor of the Page Rank calculation. Must be in [0, 1).
+    static constexpr const char* NAME = "dampingfactor";
+    static constexpr common::LogicalTypeID TYPE = common::LogicalTypeID::DOUBLE;
+    static constexpr double DEFAULT_VALUE = 0.85;
+
+    static void validate(double dampingFactor);
+};
+
+void DampingFactor::validate(double dampingFactor) {
+    if (dampingFactor < 0 || dampingFactor >= 1) {
+        throw common::BinderException{"Damping factor must be in the [0, 1)."};
+    }
+}
+
+struct Tolerance {
+    // Minimum change in scores between iterations. If all scores change less than the tolerance
+    // value the result is considered stable and the algorithm returns.
+    static constexpr const char* NAME = "tolerance";
+    static constexpr common::LogicalTypeID TYPE = common::LogicalTypeID::DOUBLE;
+    static constexpr double DEFAULT_VALUE = 0.0000001;
+};
+
+struct PageRankConfig final : public GDSConfig {
+    double dampingFactor = DampingFactor::DEFAULT_VALUE;
+    uint64_t maxIterations = 20;
+    double tolerance = Tolerance::DEFAULT_VALUE;
+
+    PageRankConfig() = default;
+};
+
+} // namespace function
+} // namespace kuzu
